bd58fd2e369840e10c456076aae63f840fdfb9d2
Andrii Veremii <dashuk10@gmail.com>
2023-10-17 22:36:44 +0200

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,89 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+function markupGallery(arrImages) {
+  return arrImages
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join('');
+}
+
+const galleryList = document.querySelector('.js-gallery');
+galleryList.insertAdjacentHTML('beforeend', markupGallery(galleryItems));
+galleryList.addEventListener('click', onClick);
+
+//------- Variant 1 ---------
+
+function onClick(e) {
+  e.preventDefault();
+
+  const isImages = e.target.classList.contains('gallery__image');
+  if (!isImages) {
+    return;
+  }
+
+  const items = `<img src="${e.target.dataset.source}" width="1280" alt="${e.target.dataset.description}">`;
+
+  const instance = basicLightbox.create(items, {
+    onShow: instance => {
+      window.addEventListener('keydown', e => onEscPress(e));
+    },
+    onClose: instance => {
+      window.removeEventListener('keydown', e => onEscPress(e));
+    },
+  });
+
+  instance.show();
+
+  function onEscPress(e) {
+    if (e.key !== 'Escape') {
+      return;
+    }
+    instance.close();
+  }
+}
+
+//------- Variant 2 ---------
+
+// const instance = basicLightbox.create(`<img width="1280" src="" alt="">`, {
+//   onShow: instance => {
+//     window.addEventListener('keydown', onEscPress);
+//   },
+//   onClose: instance => {
+//     window.removeEventListener('keydown', onEscPress);
+//   },
+// });
+
+// function onClick(e) {
+//   e.preventDefault();
+
+//   const isImages = e.target.classList.contains('gallery__image');
+//   if (!isImages) {
+//     return;
+//   }
+
+//   const dataSrc = e.target.dataset.source;
+//   const dataAlt = e.target.dataset.description;
+
+//   instance.element().querySelector('img').src = dataSrc;
+//   instance.element().querySelector('img').alt = dataAlt;
+
+//   instance.show();
+// }
+
+// function onEscPress(e) {
+//   if (e.code !== 'Escape') {
+//     return;
+//   }
+
+//   instance.close();
+// }
