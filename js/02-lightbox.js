4051cf0787de087503489dafd61417e4a671467e
Andrii Veremii <dashuk10@gmail.com>
2023-10-19 21:27:54 +0200

    add

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.js-gallery');
+
+galleryList.innerHTML = galleryItems.map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join('');
+
+ var lightbox = new SimpleLightbox('.gallery a', {
+   captionsData: 'alt',
+   captionDelay: 250,
+   captionPosition: 'bottom',
+ });
+
+
+//------- Var 2 ---------
+
+
+// function markupGallery(arrImages) {
+//   return arrImages
+//     .map(
+//       ({ preview, original, description }) => `<li class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </li>`
+//     )
+//     .join('');
+// }
+
+// galleryList.insertAdjacentHTML('beforeend', markupGallery(galleryItems));
+
+//  var lightbox = new SimpleLightbox('.gallery a', {
+//    captionsData: 'alt',
+//    captionDelay: 250,
+//    captionPosition: 'bottom',
+//  });
